c45e8139e7b1d1defe067826a894f7ae22944ca6
TntTastisch <tnttastisch@gmail.com>
2022-11-15 20:37:11 +0100

    Added displayname

--- src/main/java/net/galacticprojects/spigot/listener/ConnectionListener.java
+++ src/main/java/net/galacticprojects/spigot/listener/ConnectionListener.java
@@ -1,19 +1,38 @@
 package net.galacticprojects.spigot.listener;
 
+import com.mojang.authlib.GameProfile;
+import eu.cloudnetservice.driver.CloudNetDriver;
+import eu.cloudnetservice.driver.permission.PermissionGroup;
+import eu.cloudnetservice.driver.permission.PermissionUser;
+import net.galacticprojects.common.util.MojangProfileService;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import javax.swing.*;
+import java.lang.reflect.Field;
+import java.util.UUID;
 
 public class ConnectionListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-
+        final UUID uniqueId = event.getPlayer().getUniqueId();
+        final String name = MojangProfileService.getName(uniqueId);
+        final PermissionUser permissionUser = CloudNetDriver.instance().permissionManagement().user(uniqueId);
+        final String tag = ChatColor.translateAlternateColorCodes('&', CloudNetDriver.instance().permissionManagement().highestPermissionGroup(permissionUser).prefix() + " " + name);
+        event.getPlayer().setDisplayName(fixName(tag));
     }
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-
+    private String fixName(String value) {
+        if (value.length() < 16)
+            return value;
+        return value.substring(0, 16);
     }
 }
